94d95f35bd8d46fc705024810a703bd10d1a82be
John Murray <johnmurray@fastmail.com>
2021-09-12 11:18:01 -0400

    adds sql directory

---
+++ sql/create-crime-cobra21-raw.sql
@@ -0,0 +1,21 @@
+-- create raw zone COBRA 21 table
+CREATE TABLE Crime.cobra21_raw (
+    id INTEGER NOT NULL IDENTITY(1,1)
+    , offense_id varchar(50)
+    , rpt_date varchar(50)
+    , occur_date varchar(50)
+    , occur_day varchar(50)
+    , occur_day_num varchar(50)
+    , occur_time varchar(50)
+    , poss_date varchar(50)
+    , poss_time varchar(50)
+    , beat varchar(50)
+    , zone varchar(50)
+    , location varchar(50)
+    , ibr_code varchar(50)
+    , UC2_Literal varchar(50)
+    , neighborhood varchar(50)
+    , npu varchar(50)
+    , lat varchar(50)
+    , long varchar(50)
+);
