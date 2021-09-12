4764875d23778b636e5cae56c52a44c4e1bbc4a0
John Murray <johnmurray@fastmail.com>
2021-09-12 11:59:28 -0400

    adds notebook with crud commands

--- sql/create-crime-cobra21-raw.sql
+++ sql/create-crime-cobra21-raw.sql
@@ -1,4 +1,8 @@
 -- create raw zone COBRA 21 table
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Crime.cobra21_raw') AND type in (N'U'))
+DROP TABLE Crime.cobra21_raw
+
 CREATE TABLE Crime.cobra21_raw (
     id INTEGER NOT NULL IDENTITY(1,1)
     , offense_id varchar(50)
@@ -11,7 +15,7 @@ CREATE TABLE Crime.cobra21_raw (
     , poss_time varchar(50)
     , beat varchar(50)
     , zone varchar(50)
-    , location varchar(50)
+    , location varchar(250)
     , ibr_code varchar(50)
     , UC2_Literal varchar(50)
     , neighborhood varchar(50)
